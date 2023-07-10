1dfa0b42f4c9a3a03a9d7cdacd1943987dc71780
helbertm <helbertm@proton.me>
2023-07-10 17:18:26 -0300

    feat: retiradas as "firulas" da brincadeira inicial.

--- sorteio-livros-implantacao.py
+++ sorteio-livros-implantacao.py
@@ -11,29 +11,16 @@ def realizar_sorteio(lista_nomes):
     lista_nomes.remove(primeiro_ganhador)
     
     # Exibe o primeiro ganhador
-    print("Checando se todas as pessoas curtiram o post oficial...........")
-    time.sleep(3)
-    limpa_tela()
-    print("Checando se todas as pessoas curtiram o post oficial...........OK")
-    
-    print("Checando se todas as pessoas estão seguindo o perfil...........")
-    time.sleep(3)
-    limpa_tela()
-    print("Checando se todas as pessoas curtiram o post oficial...........OK")
-    print("Checando se todas as pessoas estão seguindo o perfil...........OK!\n\n\n")
-    print("Fazendo o primeiro sorteio\n")
-    time.sleep(5)
-
-    print(primeiro_ganhador, "ganhou o livro: Estatística - O que é!\n\n")
+    print(primeiro_ganhador, " - Ganhou o primeiro sorteio!\n\n")
     
     # Exibe a mensagem de pausa
     print("Iniciando próximo sorteio com a nova lista:", lista_nomes,"\n\n")
-    time.sleep(3)
+    time.sleep(5)
 
     # Sorteia o segundo ganhador
     segundo_ganhador = random.choice(lista_nomes)
 
-    print("Quem levou o livro 'Como Mentir com Estatística' para casa foi a(o) ", segundo_ganhador,"\n\n\n")
+    print(segundo_ganhador, " - Ganhou o segundo sorteio!\n\n")
     
     # Retorna os nomes dos ganhadores
     return primeiro_ganhador, segundo_ganhador
@@ -53,23 +40,21 @@ def limpa_tela():
     os.system('cls' if os.name == 'nt' else 'clear') 
 
 
-os.system('cls' if os.name == 'nt' else 'clear')
+limpa_tela()
 lista_pessoas = []
-print("\n\n================= O  SORTEIO  DO  ANO =================\n\n")
+
 while True:
-   
-    nome = input("Digite o nome da pessoa ou <enter> para fazer o sorteio: ")
+    nome = input("Digite o nome da pessoa ou <enter> para iniciar o sorteio: ")
     if not nome:
         break
     elif nome.upper() in lista_pessoas:
-        print("Nome já existe na lista. Tente novamente.")
+        print("Nome já incluído na lista.")
     else:
         lista_pessoas.append(nome.upper())
         limpa_tela()
-        print("\n\n================= O  SORTEIO  DO  ANO =================\n\n")
         print("Lista de nomes atualizada:", lista_pessoas,"\n\n")  # Exibe a lista
         chance_sorteio_1, chance_sorteio_2 = calcular_probabilidade(lista_pessoas)
-        print(f"Cada pessoa tem {chance_sorteio_1:.2f}% de chance de levar um livro para casa no primeiro sorteio e {chance_sorteio_2:.2f}% no segundo sorteio.\n\n")
+        print(f"Cada uma das {len(lista_pessoas)} pessoa(s) participante(s) tem {chance_sorteio_1:.2f}% de chance de levar um livro para casa no primeiro sorteio e {chance_sorteio_2:.2f}% no segundo sorteio.\n\n")
 
 # Limpa a tela
 limpa_tela()
@@ -78,4 +63,4 @@ limpa_tela()
 ganhador1, ganhador2 = realizar_sorteio(lista_pessoas)
 
 # Exibe os ganhadores
-print("Parabéns: {} e {}!".format(ganhador1, ganhador2), "Os livros serão enviados para vocês gratuitamente, somente as *** taxas de manuseio de R$ 257,89 *** ficam por conta de vocês.\n\n\n")
+print("Parabéns: {} e {}!".format(ganhador1, ganhador2), "<mensagem_aos_ganhadores>")
